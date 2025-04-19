820989477acaf55383b34f702eb7270cd807fac8
Dr Heart Success💫 <drheartsuccess021@gmail.com>
2025-04-19 20:32:59 +1200

    add autocommands

--- init.lua
+++ init.lua
@@ -5,19 +5,7 @@ vim.g.have_nerd_font = true
 
 require 'options'
 require 'keymaps'
-
--- [[ Basic Autocommands ]]
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'autocommands'
 
 -- [[ Configure and install plugins ]]
 --
